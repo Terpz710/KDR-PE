e36ab227d50bf5938e416d51e5e4749edb3368fd
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-11-29 11:21:25 -0800

    TopKill fix

--- src/Terpz710/KDRPE/Command/TopKillCommand.php
+++ src/Terpz710/KDRPE/Command/TopKillCommand.php
@@ -14,53 +14,25 @@ class TopKillCommand extends Command {
 
     private $plugin;
 
-    public function __construct($plugin) {
-        parent::__construct('topkill', 'Display top kills', '/topkill');
+    public function __construct(Main $plugin) {
+        parent::__construct('topkill', 'Shows top kills', '/topkill');
         $this->setPermission('kdr-pe.topkill');
         $this->plugin = $plugin;
     }
 
-    public function execute(CommandSender $sender, string $commandLabel, array $args): void {
-        if ($sender instanceof Player && !$this->testPermission($sender)) {
-            return;
-        }
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
+        $topKills = $this->plugin->getTopKills();
 
-        $topKillList = $this->getTopKillList();
+        if (!empty($topKills)) {
+            $sender->sendMessage("Top Kills:");
 
-        if (empty($topKillList)) {
-            $sender->sendMessage('No top kills available.');
-            return;
-        }
-
-        $sender->sendMessage('Top Kills:');
-        $count = 0;
-        foreach ($topKillList as $position => $playerName) {
-            $kills = $this->plugin->getKills($playerName);
-            $sender->sendMessage("[{$position}] {$playerName} - Kills: {$kills}");
-
-            $count++;
-            if ($count >= 10) {
-                break;
+            foreach ($topKills as $playerName => $kills) {
+                $sender->sendMessage("{$playerName}: {$kills}");
             }
-        }
-    }
-
-    private function getTopKillList(): array {
-        $topKillList = [];
-        $playerData = $this->plugin->getPlayerData();
-
-        uasort($playerData, function ($a, $b) {
-            return $this->plugin->getKills($b) <=> $this->plugin->getKills($a);
-        });
-
-        $count = 0;
-        foreach ($playerData as $playerName) {
-            $topKillList[$count] = $playerName;
-            if ($count >= 10) {
-                break;
-            }
+        } else {
+            $sender->sendMessage("No top kills yet.");
         }
 
-        return $topKillList;
+        return true;
     }
 }
