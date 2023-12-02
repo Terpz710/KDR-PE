e4dab9893737376c1482681ef73b9e9ed0a1ae1b
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-12-02 01:44:27 -0800

    Fix

--- src/Terpz710/KDRPE/Main.php
+++ src/Terpz710/KDRPE/Main.php
@@ -2,103 +2,38 @@
 
 declare(strict_types=1);
 
-namespace Terpz710\KDRPE;
+namespace Terpz710\KDRPE\Command;
 
-use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
-use pocketmine\event\entity\EntityDamageByEntityEvent;
-use pocketmine\event\player\PlayerDeathEvent;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\player\Player;
-use pocketmine\utils\Config;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 
-use Terpz710\KDRPE\Command\KDRCommand;
-use Terpz710\KDRPE\Command\TopKillCommand;
+use Terpz710\KDRPE\Main;
 
-class Main extends PluginBase implements Listener {
+class TopKillCommand extends Command {
 
-    public function onEnable(): void {
-        $this->getServer()->getCommandMap()->register('kdr', new KDRCommand($this));
-        $this->getServer()->getCommandMap()->register('topkill', new TopKillCommand($this));
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    private $plugin;
 
-        if (!file_exists($this->getDataFolder() . 'data.yml')) {
-            $this->saveResource('data.yml');
-        }
+    public function __construct(Main $plugin) {
+        parent::__construct('topkill', 'Shows top kills', '/topkill');
+        $this->setPermission('kdr-pe.topkill');
+        $this->plugin = $plugin;
     }
 
-    public function onDeath(PlayerDeathEvent $event): void {
-        $player = $event->getPlayer();
-        $this->initializePlayerData($player->getName());
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
+        $topKills = $this->plugin->getTopKills();
 
-        $cause = $player->getLastDamageCause();
+        if (!empty($topKills)) {
+            $sender->sendMessage("----§eTop Kills§f----");
 
-        if ($cause instanceof EntityDamageByEntityEvent) {
-            $damager = $cause->getDamager();
-
-            if ($damager instanceof Player) {
-                $this->incrementKill($damager->getName());
+            $rank = 1;
+            foreach ($topKills as $playerName => $kills) {
+                $sender->sendMessage("{$rank}. {$playerName} - {$kills} Kills");
+                $rank++;
             }
+        } else {
+            $sender->sendMessage("No top kills yet.");
         }
 
-        $this->incrementDeath($player->getName());
-    }
-
-    public function onPlayerJoin(PlayerJoinEvent $event): void {
-        $player = $event->getPlayer();
-        $playerName = $player->getName();
-
-        $this->initializePlayerData($playerName);
-    }
-
-    private function initializePlayerData(string $playerName): void {
-        $config = new Config($this->getDataFolder() . 'data.yml', Config::YAML);
-        if (!$config->exists($playerName . '.kills') || !$config->exists($playerName . '.deaths')) {
-            $config->set($playerName . '.kills', 0);
-            $config->set($playerName . '.deaths', 0);
-            $config->save();
-        }
-    }
-
-    private function incrementKill(string $playerName): void {
-        $config = new Config($this->getDataFolder() . 'data.yml', Config::YAML);
-        $kills = $config->get($playerName . '.kills', 0);
-        $config->set($playerName . '.kills', ++$kills);
-        $config->save();
-    }
-
-    private function incrementDeath(string $playerName): void {
-        $config = new Config($this->getDataFolder() . 'data.yml', Config::YAML);
-        $deaths = $config->get($playerName . '.deaths', 0);
-        $config->set($playerName . '.deaths', ++$deaths);
-        $config->save();
-    }
-
-    public function getPlayerData(): array {
-        $config = new Config($this->getDataFolder() . 'data.yml', Config::YAML);
-        return $config->getAll();
-    }
-
-    public function getKills(string $playerName): int {
-        $config = new Config($this->getDataFolder() . 'data.yml', Config::YAML);
-        return $config->get($playerName . '.kills', 0);
-    }
-
-    public function getDeaths(string $playerName): int {
-        $config = new Config($this->getDataFolder() . 'data.yml', Config::YAML);
-        return $config->get($playerName . '.deaths', 0);
-    }
-
-    public function getTopKills(): array {
-        $playerData = $this->getPlayerData();
-        $topKills = [];
-
-        foreach ($playerData as $playerName => $data) {
-            $kills = $data['kills'] ?? 0;
-            $topKills[$playerName] = $kills;
-        }
-
-        arsort($topKills);
-        return array_slice($topKills, 0, 5);
+        return true;
     }
 }
