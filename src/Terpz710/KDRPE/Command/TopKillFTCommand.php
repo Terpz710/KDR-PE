2b1c359fffea4145c7f7b14651b5c7dc08f6b7a6
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-12-15 01:22:06 -0800

    Update TopKillFTCommand.php

--- src/Terpz710/KDRPE/Command/TopKillFTCommand.php
+++ src/Terpz710/KDRPE/Command/TopKillFTCommand.php
@@ -69,11 +69,15 @@ class TopKillFTCommand extends Command {
         $tag = 'topkill';
         FloatingKDRAPI::create($position, $tag, $text, $this->plugin->getDataFolder());
         FloatingKDRAPI::saveToFile($this->plugin->getDataFolder() . 'FT' . DIRECTORY_SEPARATOR . 'floating_text_data.json');
+        $sender->sendMessage('§l(§a!§f)§r§f Top Kill leaderboard Floating Text created!');
+        return true;
     }
 
     private function removeFloatingText(Player $player): void {
         $tag = 'topkill';
         FloatingKDRAPI::remove($tag, $this->plugin->getDataFolder() . 'FT')
         FloatingKDRAPI::saveToFile($this->plugin->getDataFolder() . 'FT' . DIRECTORY_SEPARATOR . 'floating_text_data.json');
+        $sender->sendMessage('§l(§c!§f)§r§f Top Kill leaderboard Floating Text has been turned off!');
+        return true;
     }
 }
