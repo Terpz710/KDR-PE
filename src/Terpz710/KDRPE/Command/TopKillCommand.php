9bf43a7748390c1c35d0044216ff0c0218b15381
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-12-15 12:49:25 -0800

    Update TopKillCommand.php

--- src/Terpz710/KDRPE/Command/TopKillCommand.php
+++ src/Terpz710/KDRPE/Command/TopKillCommand.php
@@ -6,17 +6,23 @@ namespace Terpz710\KDRPE\Command;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginOwned;
+use pocketmine\plugin\Plugin;
 
 use Terpz710\KDRPE\Main;
 
-class TopKillCommand extends Command {
+class TopKillCommand extends Command implements PluginOwned {
 
     private $plugin;
 
     public function __construct(Main $plugin) {
         parent::__construct('topkill', 'Shows top kills', '/topkill');
-        $this->setPermission('kdr-pe.topkill');
+        $this->setPermission('kdr-pe.command.topkill');
         $this->plugin = $plugin;
+    }
+
+    public function getOwningPlugin(): Plugin {
+        return $this->plugin;
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
@@ -35,9 +41,8 @@ class TopKillCommand extends Command {
                 }
             }
         } else {
-            $sender->sendMessage("No top kills yet.");
-        }
-
+            $sender->sendMessage("§l§f(§c!§f)§r§f No top kills yet!");
+        } // This message shouldnt get sent either way unless the json gets currupted.
         return true;
     }
 }
