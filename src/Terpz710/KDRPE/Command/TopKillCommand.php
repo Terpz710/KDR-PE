64ac98985d55a49741d42bb2472f123555638eb8
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2024-03-17 19:28:35 -0700

    Update TopKillCommand.php

--- src/Terpz710/KDRPE/Command/TopKillCommand.php
+++ src/Terpz710/KDRPE/Command/TopKillCommand.php
@@ -8,8 +8,10 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginOwned;
 use pocketmine\plugin\Plugin;
+use pocketmine\Player;
 
 use Terpz710\KDRPE\Main;
+use JoJoe77777\FormAPI\SimpleForm;
 
 class TopKillCommand extends Command implements PluginOwned {
 
@@ -26,23 +28,29 @@ class TopKillCommand extends Command implements PluginOwned {
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
-        $topKills = $this->plugin->getTopKills();
+        if ($sender instanceof Player) {
+            $topKills = $this->plugin->getTopKills();
 
-        if (!empty($topKills)) {
-            $sender->sendMessage("-----------§eTop Kills§f-----------");
-
-            $rank = 1;
-            foreach ($topKills as $playerName => $kills) {
-                $formattedName = ucwords(strtolower($playerName));
-                $sender->sendMessage("§e{$rank}. §f{$formattedName}§f: §e{$kills}");
-                $rank++;
-                if ($rank > 10) {
-                    break;
+            if (!empty($topKills)) {
+                $content = "-----------\n";
+                foreach ($topKills as $playerName => $kills) {
+                    $formattedName = ucwords(strtolower($playerName));
+                    $content .= "§e{$formattedName}§f: §e{$kills}\n";
                 }
+                $content .= "-----------";
+
+                $form = new SimpleForm(function (Player $player, ?int $data) use ($content) {
+                });
+                $form->setTitle("Top Kills");
+                $form->setContent($content);
+                $sender->sendForm($form);
+            } else {
+                $sender->sendMessage("§l§f(§c!§f)§r§f No top kills yet!"); //This message shouldnt get sent either way unless the json gets currupted.
             }
         } else {
-            $sender->sendMessage("§l§f(§c!§f)§r§f No top kills yet!");
-        } // This message shouldnt get sent either way unless the json gets currupted.
+            $sender->sendMessage("§cThis command can only be used in-game.");
+        }
+
         return true;
     }
 }
