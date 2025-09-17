f273d5a9f5124d8d343220e787c3bf30fe9ae17f
WavyCraftNetwork <142659461+Terpz710@users.noreply.github.com>
2025-09-17 13:49:00 -0700

    Update KillStreakLeaderboard.php

--- src/terpz710/kdrpe/floatingtext/leaderboard/KillStreakLeaderboard.php
+++ src/terpz710/kdrpe/floatingtext/leaderboard/KillStreakLeaderboard.php
@@ -28,7 +28,7 @@ final class KillStreakLeaderboard {
 
     public function updateKillStreakFT() : void{
         $top_killstreak = $this->getTopKillStreak();
-        $text = "§l§a-=Top KillStreak Leaderboard=-\n";
+        $text = "§l§b-=Top KillStreak Leaderboard=-\n";
 
         $rank = 1;
         foreach ($top_killstreak as $name => $killstreak) {
